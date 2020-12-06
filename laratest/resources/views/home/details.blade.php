59e1781685d90ee5903a1050cc8568223019c896
Al-sany Samil <alsanysamil@gmail.com>
2020-12-06 23:06:30 +0600

    student details

--- laratest/resources/views/home/details.blade.php
+++ laratest/resources/views/home/details.blade.php
@@ -9,19 +9,19 @@
     <table>
         <tr>
             <td><h2>ID</h2></td>
-            <td><h3>{{$student['id']}} </he></td>
+            <td><h3>{{$student['id']}} </h3></td>
         </tr>
         <tr>
             <td><h2>Name</h2></td>
-            <td><h3>{{$student['name']}} </he></td>
+            <td><h3>{{$student['name']}} </h3></td>
         </tr>
         <tr>
             <td><h2>CGPA</h2></td>
-            <td><h3>{{$student['cgpa']}} </he></td>
+            <td><h3>{{$student['cgpa']}} </h3></td>
         </tr>
         <tr>
             <td><h2>Email</h2></td>
-            <td><h3>{{$student['email']}} </he></td>
+            <td><h3>{{$student['email']}} </h3></td>
         </tr>
         <tr>
             <td><button><a href="/stdlist">Back</a></button></td>
