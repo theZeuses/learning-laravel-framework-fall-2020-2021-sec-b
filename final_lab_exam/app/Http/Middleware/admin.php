5a212ddd6c7b4e83eb156fcfe25d2a6b3ba9dfa3
Al-sany Samil <alsanysamil@gmail.com>
2020-12-23 11:56:36 +0600

    initial commit

---
+++ final_lab_exam/app/Http/Middleware/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if($request->session()->get('type') == 'admin'){
+            return $next($request);
+        }else{
+            return redirect()->route('/home');
+        }
+    }
+}
